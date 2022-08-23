45e483053a25da48a38a670027103e79bc629a11
divyagaikwad27 <111782150+divyagaikwad27@users.noreply.github.com>
2022-08-23 19:18:33 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,48 @@
+package PrimitiveDataTypes;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			    //boolean Example
+				boolean IoT=true;
+				System.out.println(IoT);
+				
+				//byte example
+				byte range;
+				range=127;
+				System.out.println(range);
+				
+				//short example
+				short range1;
+				range1=-127;
+				System.out.println(range1);
+				
+				//int example
+				int range2;
+				range2=300000000;
+				System.out.println(range2);
+				
+				//long example
+				long number=450000000000l;
+				System.out.println(number);
+				
+				//float Example
+				float number1=99.8f;
+				System.out.println(number1);
+				
+				//Char Example(unicode value of A is '\u0041')
+				char letter='\u0041';
+				System.out.println(letter);
+				
+				//char using ASCII
+				char letter1=65;//ASCII value of A is 65
+				System.out.println(letter1);
+				
+				char letter3='A';
+				System.out.println(letter3);
+				
+	}
+
+}
